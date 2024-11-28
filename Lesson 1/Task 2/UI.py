48fc1ae4528f8dc2f0d9394e11b93ce190c785cf
student <123@gmail.com>
2024-11-28 17:33:35 +0300

    First

---
+++ Lesson 1/Task 2/UI.py
@@ -0,0 +1,36 @@
+import sys
+import random
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QInputDialog
+
+
+class RandomFlag(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.base = [30, 60, 120, 30]
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 400)
+        self.setWindowTitle('Рисование')
+        self.button = QPushButton('Рисовать', self)
+        self.button.move(70, 20)
+        self.button.clicked.connect(self.paint)
+        self.num = 0
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        r = random.randint(10, 400)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(10, 10, r, r)
---
+++ Lesson 1/Task 2/main.py
@@ -0,0 +1,8 @@
+from UI import *
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = RandomFlag()
+    ex.show()
+    sys.exit(app.exec())
